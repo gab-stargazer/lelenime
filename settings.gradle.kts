6abc489c2ac29eba9d05e50df19ed74ba14fde74
kamil <km8003296@gmail.com>
2023-04-15 15:33:21 +0700

    Initial Setup

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lelenime"
+include(":app")
+include(":core:common")
+include(":core:network")
+include(":core:database")
+include(":core:model")
+include(":core:data")
+include(":core:domain")
+include(":feature:explore")
+include(":feature:collection")
+include(":feature:settings")
+include(":feature:detail")
+include(":feature:more")
