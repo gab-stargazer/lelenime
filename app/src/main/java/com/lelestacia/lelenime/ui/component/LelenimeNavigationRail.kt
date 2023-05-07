c2ef4e60304c393a985b07b0ad72477cc79c7826
LeleStacia <km8003296@gmail.com>
2023-05-07 16:25:06 +0700

    Added Navigation Rail and Animated Navigation Bar

--- app/src/main/java/com/lelestacia/lelenime/ui/component/LelenimeNavigationRail.kt
+++ app/src/main/java/com/lelestacia/lelenime/ui/component/LelenimeNavigationRail.kt
@@ -2,40 +2,54 @@ package com.lelestacia.lelenime.ui.component
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.lelestacia.lelenime.core.common.navItem
+import com.lelestacia.lelenime.core.common.route.Screen
 import com.lelestacia.lelenime.util.rootDestinations
 
 @Composable
-fun LeleNimeBottomBar(navController: NavHostController) {
+fun LelenimeNavigationRail(navController: NavHostController) {
     val navBackStackEntry: NavBackStackEntry? by navController.currentBackStackEntryAsState()
     val currentRoute: String? = navBackStackEntry?.destination?.route
-    val shouldBeVisible = rootDestinations.contains(currentRoute)
+    val shouldBeVisible = rootDestinations.contains(currentRoute ?: Screen.Explore.route)
 
     AnimatedVisibility(
         visible = shouldBeVisible,
-        enter = slideInVertically(
-            initialOffsetY = { fullHeight -> fullHeight },
+        enter = slideInHorizontally(
+            initialOffsetX = { fullWidth -> -fullWidth },
             animationSpec = tween(durationMillis = 250)
         ),
-        exit = slideOutVertically(
-            targetOffsetY = { fullHeight -> fullHeight },
+        exit = slideOutHorizontally(
+            targetOffsetX = { fullWidth -> -fullWidth },
             animationSpec = tween(durationMillis = 250)
         )
     ) {
-        NavigationBar {
+        NavigationRail(
+            header = {
+                Image(
+                    painter = painterResource(id = com.lelestacia.lelenime.core.common.R.drawable.lelenime),
+                    contentDescription = "Lelenime Icon",
+                    modifier = Modifier.size(48.dp)
+                )
+            }
+        ) {
             navItem.map { navItem ->
-                NavigationBarItem(
+                NavigationRailItem(
                     selected = currentRoute == navItem.screen.route,
                     onClick = {
                         navController.navigate(route = navItem.title) {
